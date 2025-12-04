4e2bfead4735730dd0912717c16deb7b0f437143
Trung Vu <trung_vu@datahouse.com>
2025-12-04 16:36:23 +0700

    fix: fix networking issue

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -106,7 +106,7 @@ resource "aws_ecs_service" "app" {
   network_configuration {
     security_groups  = [aws_security_group.ecs_tasks.id]
     subnets          = data.aws_subnets.private.ids
-    assign_public_ip = false
+    assign_public_ip = true
   }
 
   load_balancer {
